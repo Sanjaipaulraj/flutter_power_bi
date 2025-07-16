7d2fd24f62a6ba84dd30c913dc9d8d49663b20b2
Sanjai <sanjaipaulraj03@gmail.com>
2025-07-16 12:17:35 +0530

    draggable power bi

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,8 +9,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool isSidebarVisible = true;
-  double sidebarWidth = 300;
+  // bool isSidebarVisible = true;
+  // double sidebarWidth = 300;
+  bool isFilterPane = true;
+  bool isVisualPane = true;
+  double filterbarWidth = 300;
+  double visualbarWidth = 300;
 
   @override
   Widget build(BuildContext context) {
@@ -52,11 +56,84 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Container(color: Colors.grey[100], child: const DraggableDashboard()),
                 ),
 
+                // AnimatedContainer(
+                //   duration: const Duration(milliseconds: 300),
+                //   width: isSidebarVisible ? sidebarWidth : 0,
+                //   color: Colors.white,
+                //   child: (isSidebarVisible && sidebarWidth > 100)
+                //       ? LayoutBuilder(
+                //           builder: (context, constraints) {
+                //             if (constraints.maxWidth < 100) {
+                //               return const SizedBox();
+                //             }
+
+                //             return Column(
+                //               crossAxisAlignment: CrossAxisAlignment.stretch,
+                //               children: [
+                //                 Container(
+                //                   color: const Color.fromARGB(255, 61, 54, 54),
+                //                   height: 50,
+                //                   child: const Center(
+                //                     child: Text(
+                //                       "Filters Pane",
+                //                       style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                //                     ),
+                //                   ),
+                //                 ),
+                //                 Expanded(
+                //                   child: Padding(
+                //                     padding: const EdgeInsets.all(8.0),
+                //                     child: ListView(
+                //                       children: List.generate(5, (index) {
+                //                         return Card(
+                //                           margin: const EdgeInsets.symmetric(vertical: 8),
+                //                           child: Container(
+                //                             padding: const EdgeInsets.all(8.0),
+                //                             child: Row(
+                //                               crossAxisAlignment: CrossAxisAlignment.start,
+                //                               children: [
+                //                                 const Icon(Icons.filter_alt),
+                //                                 const SizedBox(width: 8),
+                //                                 Expanded(
+                //                                   child: Column(
+                //                                     crossAxisAlignment: CrossAxisAlignment.start,
+                //                                     children: const [
+                //                                       Text(
+                //                                         "Filter title",
+                //                                         style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+                //                                         overflow: TextOverflow.ellipsis,
+                //                                         maxLines: 1,
+                //                                       ),
+                //                                       Text(
+                //                                         "Filter details here",
+                //                                         style: TextStyle(fontWeight: FontWeight.normal),
+                //                                         overflow: TextOverflow.ellipsis,
+                //                                         maxLines: 2,
+                //                                       ),
+                //                                     ],
+                //                                   ),
+                //                                 ),
+                //                               ],
+                //                             ),
+                //                           ),
+                //                         );
+                //                       }),
+                //                     ),
+                //                   ),
+                //                 ),
+                //               ],
+                //             );
+                //           },
+                //         )
+                //       : null,
+                // ),
+
+                //Filter Animated Container
                 AnimatedContainer(
                   duration: const Duration(milliseconds: 300),
-                  width: isSidebarVisible ? sidebarWidth : 0,
+                  width: isFilterPane ? filterbarWidth : 0,
                   color: Colors.white,
-                  child: (isSidebarVisible && sidebarWidth > 100)
+                  child: (isFilterPane && filterbarWidth > 100)
                       ? LayoutBuilder(
                           builder: (context, constraints) {
                             if (constraints.maxWidth < 100) {
@@ -124,11 +201,11 @@ class _HomeScreenState extends State<HomeScreen> {
                       : null,
                 ),
 
-                // Toggle Sidebar Button
+                //Filter Container toggle button
                 GestureDetector(
                   onTap: () {
                     setState(() {
-                      isSidebarVisible = !isSidebarVisible;
+                      isFilterPane = !isFilterPane;
                     });
                   },
                   child: MouseRegion(
@@ -142,12 +219,155 @@ class _HomeScreenState extends State<HomeScreen> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            Icon(isSidebarVisible ? Icons.arrow_forward_ios : Icons.arrow_back_ios, size: 16),
-                            if (isSidebarVisible != true)
+                            Icon(isFilterPane ? Icons.arrow_forward_ios : Icons.arrow_back_ios, size: 16),
+                            if (isFilterPane != true)
                               RotatedBox(
                                 quarterTurns: 1,
                                 child: Text(
                                   "Filters",
+                                  style: TextStyle(fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                //Visual Animated Container
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  width: isVisualPane ? visualbarWidth : 0,
+                  color: Colors.white,
+                  child: (isVisualPane && visualbarWidth > 100)
+                      ? LayoutBuilder(
+                          builder: (context, constraints) {
+                            if (constraints.maxWidth < 100) {
+                              return const SizedBox();
+                            }
+
+                            return Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Container(
+                                  color: const Color.fromARGB(255, 61, 54, 54),
+                                  height: 50,
+                                  child: const Center(
+                                    child: Text(
+                                      "Visual Pane",
+                                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: ListView(
+                                      children: List.generate(5, (index) {
+                                        return Card(
+                                          margin: const EdgeInsets.symmetric(vertical: 8),
+                                          child: Container(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Row(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                const Icon(Icons.filter_alt),
+                                                const SizedBox(width: 8),
+                                                Expanded(
+                                                  child: Column(
+                                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                                    children: const [
+                                                      Text(
+                                                        "Visual title",
+                                                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+                                                        overflow: TextOverflow.ellipsis,
+                                                        maxLines: 1,
+                                                      ),
+                                                      Text(
+                                                        "Visual details here",
+                                                        style: TextStyle(fontWeight: FontWeight.normal),
+                                                        overflow: TextOverflow.ellipsis,
+                                                        maxLines: 2,
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        );
+                                      }),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        )
+                      : null,
+                ),
+
+                // Toggle Sidebar Button
+                // GestureDetector(
+                //   onTap: () {
+                //     setState(() {
+                //       isSidebarVisible = !isSidebarVisible;
+                //     });
+                //   },
+                //   child: MouseRegion(
+                //     cursor: SystemMouseCursors.click,
+                //     child: Container(
+                //       width: 40,
+                //       color: Colors.grey[300],
+                //       child: Center(
+                //         child: Column(
+                //           spacing: 10,
+                //           mainAxisAlignment: MainAxisAlignment.center,
+                //           crossAxisAlignment: CrossAxisAlignment.center,
+                //           children: [
+                //             Icon(isSidebarVisible ? Icons.arrow_forward_ios : Icons.arrow_back_ios, size: 16),
+                //             if (isSidebarVisible != true)
+                //               RotatedBox(
+                //                 quarterTurns: 1,
+                //                 child: Text(
+                //                   "Filters",
+                //                   style: TextStyle(fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold),
+                //                   overflow: TextOverflow.ellipsis,
+                //                 ),
+                //               ),
+                //           ],
+                //         ),
+                //       ),
+                //     ),
+                //   ),
+                // ),
+
+                // Visual Container toggle button
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isVisualPane = !isVisualPane;
+                    });
+                  },
+                  child: MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: Container(
+                      width: 40,
+                      color: Colors.grey[300],
+                      child: Center(
+                        child: Column(
+                          spacing: 10,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(isVisualPane ? Icons.arrow_forward_ios : Icons.arrow_back_ios, size: 16),
+                            if (isVisualPane != true)
+                              RotatedBox(
+                                quarterTurns: 1,
+                                child: Text(
+                                  "Visuals",
                                   style: TextStyle(fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold),
                                   overflow: TextOverflow.ellipsis,
                                 ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_power_bi/draggable_dashboard.dart';
 import 'package:flutter_power_bi/home_screen.dart';
 
 void main() {
-  // runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: DraggableDashboard()));
   runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen()));
+  // runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
